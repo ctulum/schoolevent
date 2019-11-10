100dbc3996afbea221dcc263f9d0f777007062d8
ctulum <cahittulum@gmail.com>
2019-11-10 21:56:02 +0300

    Add user model

---
+++ app/src/main/java/com/example/schoolevents/Models/User.java
@@ -0,0 +1,58 @@
+package com.example.schoolevents.Models;
+
+public class User {
+
+    private String id;
+    private String givenName;
+    private String familyName;
+    private String email;
+    private String password;
+
+    private static User user;
+
+    public static void init(){
+        if(user != null) return;
+
+        user = new User();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
